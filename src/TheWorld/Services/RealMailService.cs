55559e3f11f61f7ba9a816c7b7247c7d5188eeae
Abderrahman Cheddir <cheddirabderrahman@gmail.com>
2016-05-21 14:59:11 +0100

    [Services][Mail] adding real mail service

---
+++ src/TheWorld/Services/RealMailService.cs
@@ -0,0 +1,14 @@
+﻿namespace TheWorld.Services
+{
+    public class RealMailService : IMailService
+    {
+        #region Implementation of IMailService
+
+        public bool SendMail(string to, string @from, string subject, string body)
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
